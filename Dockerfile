87bc1aedabf9239a3cb3407bed1d2f0648144283
Jae Kim <jaekim@Jaes-MacBook-Air.local>
2022-08-01 19:23:02 -0400

    edited table structure

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM debezium/example-mysql:1.6
+FROM debezium/example-mysql:1.5
 ENV MYSQL_DATABASE tweetdata
 COPY ./sql/ /docker-entrypoint-initdb.d/
--- sql/setupDB.sql
+++ sql/setupDB.sql
@@ -2,4 +2,5 @@ CREATE TABLE tweets (
     recordId INT NOT NULL AUTO_INCREMENT
     ,dataId VARCHAR(20)
     ,dataText VARCHAR(500)
+    ,PRIMARY KEY (recordId)
 );
