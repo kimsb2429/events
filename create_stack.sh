392795a56bd0f77d4a4f51241462b32f42e673d3
Jae Kim <jaekim@jaes-air.mynetworksettings.com>
2022-08-01 21:47:52 -0400

    Improved CI

--- create_stack.sh
+++ create_stack.sh
@@ -1,5 +1,6 @@
-read -p "Stack Name: " stack_name
-read -p "Template Filepath (your-template.yaml): " template_filepath
+read -p "Stack Name: " stack_name || stack_name='wcd-final-stack'
+read -p "Template Filepath (your-template.yaml): " template_filepath || template_filepath='wcd-final-stack.yaml'
+read -p "NiFi Template Filepath (nifi-template.xml): " nifi_template_filepath || nifi_template_filepath='twitter.xml'
 aws cloudformation delete-stack --stack-name $stack_name 
 aws cloudformation wait stack-delete-complete --stack-name $stack_name
 aws cloudformation create-stack --stack-name $stack_name --template-body file://$template_filepath
@@ -7,5 +8,14 @@ aws cloudformation wait stack-create-complete --stack-name $stack_name
 sleep 60
 ip=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='wcd-final-stack'].Outputs[].OutputValue" --output text)
 echo $ip
+response=$(curl --write-out '%{http_code}' --silent --output /dev/null http://107.23.254.55:8080)
+while [ $response -ne '200' ]
+do
+    sleep 10
+    response=$(curl --write-out '%{http_code}' --silent --output /dev/null http://107.23.254.55:8080)
+    echo $response
+done
+process_group_id=$(curl http://107.23.254.55:8080/nifi-api/flow/process-groups/root | jq -r '.processGroupFlow.id')
+curl -k -F template=@$nifi_template_filepath -X POST http://107.23.254.55:8080/nifi-api/process-groups/$process_group_id/templates/upload
 open http://$ip:8080/nifi/
 ssh -i prosody.pem ec2-user@$ip
